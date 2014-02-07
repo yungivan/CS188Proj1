93d2b806f7f48adca7081f135e475639bce272b3
Ivan Yung <ivannyung@gmail.com>
2014-02-06 23:10:36 -0800

    qu7 4/4

--- searchAgents.py
+++ searchAgents.py
@@ -414,8 +414,6 @@ def cornersHeuristic(state, problem):
     "*** YOUR CODE HERE ***"
     if problem.isGoalState(state):
         return 0
-
-
     
     corner1 = corners[0]
     corner2 = corners[1]
@@ -433,43 +431,11 @@ def cornersHeuristic(state, problem):
     distance = [dto1, dto2, dto3, dto4]
     total = 0 
     
-    """
-    for i in distance:
-        #print "length: %s" % len(distance)
-        if bools[i]:
-            bools.remove(bools[i])
-            distance.remove(distance[i])
-        print "bool: %s" % bools
-        print "distance: %s" % distance
-        print "i: %s" % i
-    for i in range(0, len(distance)):
-        total += distance[i]
-        
-    return total/len(distance)
-    """
     for i in range(0,4): 
         if bools[i]: 
             distance[i] = 0
             count -= 1
-    """
-    if v2: 
-        dto2 = 0
-        count -=1
-    if v3:
-        dto3 = 0
-        count -=1
-    if v4: 
-        dto4 = 0
-        count -=1
-    #if count == 0:
-     #   return 0
-    """
     return sum(distance)/count
-    
-    
-    
-
-
     #return 0 # Default to trivial solution
 
 class AStarCornersAgent(SearchAgent):
@@ -562,27 +528,59 @@ def foodHeuristic(state, problem):
     position, foodGrid = state
     "*** YOUR CODE HERE ***"
     #return mazeDistance(position, )
-    #return 0
-    
+    #return 0 
+    foodlist = foodGrid.asList()
+    #print "foodGridaslist: %s" %foodlist
+    #print "foodGrid: %s" %foodGrid
     if problem.isGoalState(state):
         return 0
-    
+    count = 0
+    total = 0
+    for foodpos in foodlist:
+        #print "~~~"
+        #print foodpos
+        #x, y = foodlist[i]
+        
 
+        key = str((position, foodpos))
+        if key in problem.heuristicInfo: 
+            total+= problem.heuristicInfo[key] 
+            count+=1
+        else:
+            pos = mazeDistance(position, foodpos, problem.startingGameState)
+            problem.heuristicInfo[key] = pos
+            total += pos
+            count += 1
+    #print total/count
+    return total/count
+    """
+    for x in range(0, len(foodGrid)):
+        print x
+        for y in range(0, len(foodGrid[x])):
+            if foodlist[x][y]:
+                foodlist[x][y]
+    """
+
+    """
+    position,v1,v2,v3,v4=state 
+    
+    bools = [v1,v2,v3,v4]
 
     dto1 = mazeDistance(position, corner1, problem.startingGameState)
-    dto2 = mazeDistance(position, corner2,problem.startingGameState)
-    dto3 = mazeDistance(position, corner3,problem.startingGameState)
-    dto4 = mazeDistance(position,corner4,problem.startingGameState)
-    return min(dto1, dto2, dto3, dto4) 
+    dto2 = mazeDistance(position, corner2, problem.startingGameState)
+    dto3 = mazeDistance(position, corner3, problem.startingGameState)
+    dto4 = mazeDistance(position, corner4, problem.startingGameState)
+    count = 4
+    #print dto1, dto2, dto3, dto4
+    distance = [dto1, dto2, dto3, dto4]
+    total = 0 
+    
+    for i in range(0,4): 
+        if bools[i]: 
+            distance[i] = 0
+            count -= 1
+    return sum(distance)/count
     """
-    corner1 = self.corners[0]
-    corner2 = self.corners[1]
-    corner3 = self.corners[2]
-    corner4 = self.corners[3]
-    print corner1, corner2, corner3, corner4
-    return min(mazeDistance(position, corner1), mazeDistance(position, corner2), mazeDistance(position, corner3), mazeDistance(position,corner4))
-    """
-
 class ClosestDotSearchAgent(SearchAgent):
     "Search for all food using a sequence of searches"
     def registerInitialState(self, state):
